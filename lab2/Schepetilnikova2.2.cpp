6de4b184611cfdc6b0e2270d895d61a2d51889db
unknown <89030541016@mail.ru>
2024-10-15 11:03:57 +0300

    my_commit

--- lab2/Schepetilnikova2.2.cpp
+++ lab2/Schepetilnikova2.2.cpp
@@ -1,74 +1,38 @@
-﻿// Первый режим
-/*
-#include<stdio.h>
+﻿#include<stdio.h>
+#include<math.h>
 #include<locale.h>
-#include<stdlib.h>
-#include<time.h>
 
 void main() {
 	setlocale(LC_ALL, "rus");
-	int cm, pl, n{};
-	srand(time(NULL));
-	cm = 1 + rand() % 1001;
-	printf("Программа загадала число от 1 до 1000. Попробуйте угадать число!\n");
-	scanf_s("%d", &pl);
-	if (pl == cm) {
-		printf("Ура! Вы угадали с первого раза!\n");
-		printf("Число ваших попыток: %d\n", n = 1);
-	}
-	else {
-		while (pl != cm) {
-			printf("Попробуйте ещё!\n");
-			scanf_s("%d", &pl);
-			n = n + 1;
-			if (pl < cm)
-				printf("Загаданное число больше, чем ваше\n");
-			else if (pl > cm)
-				printf("Загаданное число меньше, чем ваше\n");
-			else
-				printf("Вы угадали!\n");
-		}
-	}
-	printf("Число ваших попыток: %d\n", n+1);
-}
-*/
-
-// Второй режим
-
-#include<stdio.h>
-#include<locale.h>
-#include<stdlib.h>
-#include<time.h>
-
-void main() {
-	setlocale(LC_ALL, "rus");
-	int pl, cm, n = 1, p;
+	int x,y = 500, b1 = 0, b2=500, b3=1001, n =1;
 	char z;
 	printf("Загадайте число от 1 до 1000.\n");
-	scanf_s("%d", &pl);
-	srand(time(NULL));
-	cm = 1 + rand() % 1001;
-	printf("%d\n", cm);
-
-	while (cm != pl) {
-		printf("Введите знак: \n");
-		scanf_s(" %c", &z);
-		n = n + 1;
-		if (z == '>') {
-			p = cm;
-			cm = p + 1 + rand() % 1001;
-			printf("%d\n", cm);
+	printf("Если число меньше, введите <\n");
+	printf("Если число больше, введите >\n");
+	printf("Если число угадано, введите =\n");
+	printf("Ваше число %d ?\n", y);
+	printf("Введите сравнение: \n");
+	printf("%c\n",&z);
+	z = getchar();
+	while (z != '=') {
+		n += 1;
+		if (z == '<') {
+			x = (b1+b2)/2;
+			y = x;
+			b3 = b2;
+			b2 = y;
 		}
-		else if (z == '<') {
-			p = cm;
-			cm = 1 + rand() % p;
-			printf("%d\n", cm);
+		else if (z == '>') {
+			x = (b2+b3)/2;
+			y = x;
+			b1 = b2;
+			b2 = y;
 		}
-		else if (z == '=') {
-			printf("Ура! Выше число: %d\n", cm);
-			break;
-		}
+		printf("Ваше число %d \n", y);
+		printf("Введите сравнение: \n");
+		scanf_s("%c", &z);
+		z = getchar();
 	}
-	printf("Число ваших попыток: %d\n",n);
-	
+	printf("Компьютер угадал!\n");
+	printf("Компьютер угадал за %d попыток\n", n);
 }
---
+++ lab3/Schepetilnikova3.cpp
@@ -0,0 +1,50 @@
+﻿#include<stdio.h>
+#include<locale.h>
+#include<stdlib.h>
+#include<time.h>
+
+void main() {
+	setlocale(LC_ALL, "rus");
+	int n, bulls = 0, cows = 0, x, pl;
+	printf("Укажите длину загадываемого числа (от 2 до 5)\n");
+	scanf_s("%d", &n);
+	int a[10] = {0,1,2,3,4,5,6,7,8,9};
+	int numbers[5], our_numbers[5];
+	srand(time(NULL));
+	x = 0 + rand() % 9;
+
+	numbers[0] = a[x];
+	a[x] = 999;
+	for (int i = 0; i < n; i++) {
+		numbers[i] = 0+ rand() % 10;
+	}
+
+	do {
+		bulls = 0; cows = 0;
+		printf("Угадай число длиной: %d\n", n);
+		scanf_s("%d", &pl);
+		if (pl == 0) {
+			printf("Конец игры! \n");
+			break;
+		}
+		for (int i = n - 1; i >= 0; i--) {
+			x = pl % 10;
+			our_numbers[i] = x;
+			pl = pl / 10;
+		}
+		for (int i = 0; i < n; i++) {
+			if (numbers[i] == our_numbers[i]) {
+				bulls++;
+			}
+			for (int j = 0; j < n; j++) {
+				if (numbers[i] == our_numbers[j])
+					cows++;
+			}
+		}
+		cows = cows - bulls;
+		printf("Количество коров: %d\n", cows);
+		printf("Количество быков: %d\n", bulls);
+		if (bulls == n)
+			printf("Ура! Вы угадали!\n");
+	} while (bulls != n);
+}
